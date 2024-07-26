088d0708a15506927aaec6ea8e1b3551dc003f75
Dimasskuy <freefiremax12d@gmail.com>
2024-07-26 18:17:17 +0700

    update jasasc.js

--- plugins/others/jasasc.js
+++ plugins/others/jasasc.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 	conn.reply(m.chat, ` Bot Ini Menggunakan Base Ori :
-• https://github.com/BOTCAHX`, m)
+• https://github.com/HelgaIlham/ZukaBet`, m)
 }
 
 handler.command = /^(sc|sourcecode)$/i
